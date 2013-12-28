9f86632c397d748bbe1e20c50585e6807d0552b0
Alex <alex@of-6.com>
2013-12-27 22:09:36 -0800

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Of6Portfolio::Application.config.secret_key_base = '7a960f76c9412964e162d5d4ee326199837e6c989de9ec22eb2560e5a597df65e9c1d5513a0e09e32d71d7cb514fca12e289ae745c7304d7b96e7f067f0869db'
